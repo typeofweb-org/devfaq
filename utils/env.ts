9cfe46174772a658678330ef38fc9f2a7c395841
Michal Miszczyszyn <michal@mmiszy.pl>
2018-05-07 14:04:53 +0200

    Add default env

--- utils/env.ts
+++ utils/env.ts
@@ -1,13 +1,25 @@
 // @ts-ignore
 const env: ProcessENV = 'undefined' !== typeof window ? (window['__ENV__'] as object) : process.env;
+
+const defaultEnv: ProcessENV = {
+  API_URL: 'https://api.localhost',
+  VERSION: 'dev',
+};
+
+// set default env
+for (const key of Object.keys(defaultEnv) as (keyof ProcessENV)[]) {
+  if (env[key] == null) {
+    env[key] = defaultEnv[key];
+  }
+}
+
 export default env;
 
-export type ProcessENV = ReturnType<typeof getEnvObjForDocument>;
+export type ProcessENV = { [K in keyof ReturnType<typeof getEnvObjForDocument>]: string };
 
 const getEnvObjForDocument = () => {
-  const { API_URL = 'https://api.localhost', VERSION = 'dev' } = process.env;
-  const env = { API_URL, VERSION };
-  return env;
+  const { API_URL, VERSION } = process.env;
+  return { API_URL, VERSION };
 };
 
 export const unsafe_getEnvScriptForDocument = () => {
