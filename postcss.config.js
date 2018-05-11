bc06e33c12b8cd701e4416bfaf485425fa858967
Michal Miszczyszyn <michal@mmiszy.pl>
2018-05-11 19:15:45 +0200

    Add postcss and autoprefixer

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+const postcssEasyImport = require('postcss-easy-import');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  plugins: [
+    postcssEasyImport({ prefix: '_' }), // keep this first
+    autoprefixer({
+      /* ...options */
+    }), // so imports are auto-prefixed too
+  ],
+};
