b7e1624cd176ffd8d62b8df72443b7042c0e54c1
Michal Miszczyszyn <michal@mmiszy.pl>
2018-05-15 17:04:23 +0200

    Selected questions list

--- components/questions/questionsListLayout/QuestionsListLayout.tsx
+++ components/questions/questionsListLayout/QuestionsListLayout.tsx
@@ -2,7 +2,7 @@ import './questionsListLayout.scss';
 
 const QuestionsListLayout: React.SFC = ({ children }) => (
   <div className="app-main-container">
-    <main className="container app-main">{children}</main>
+    <main className="container">{children}</main>
   </div>
 );
 
