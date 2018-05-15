7d4a81dfde3006a2d938dba36ec778ef612211a2
Michal Miszczyszyn <michal@mmiszy.pl>
2018-05-15 22:41:31 +0200

    Build adjustments

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const next = require('next');
 const routes = require('./routes');
 const app = next({ dev: process.env.NODE_ENV !== 'production' });
 const handler = routes.getRequestHandler(app);
+const port = process.env.PORT || 3000;
 
 // With express
 const express = require('express');
 app.prepare().then(() => {
   express()
     .use(handler)
-    .listen(3000, () => console.log('Server listening at localhost:3000'));
+    .listen(port, () => console.log('Server listening at localhost:3000'));
 });
