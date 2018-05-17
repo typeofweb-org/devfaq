ff34ceaba2d193634dbd4230cd18ba71698c1750
Michal Miszczyszyn <michal@mmiszy.pl>
2018-05-17 15:14:07 +0200

    Refactor QuestionItem

--- components/questions/questionsList/questionItem/QuestionItem.tsx
+++ components/questions/questionsList/questionItem/QuestionItem.tsx
@@ -32,54 +32,77 @@ export default class QuestionItem extends React.Component<QuestionItemOwnProps> 
             active: isSelected,
           })}
         >
-          {this.props.selectable && (
-            <input
-              onChange={() => this.props.toggleQuestion(question.id)}
-              checked={isSelected}
-              type="checkbox"
-              className="app-questions--question--checkbox"
-            />
-          )}
+          {this.maybeRenderCheckbox()}
 
           <MarkdownText className="app-questions--question--text" value={question.question} />
 
-          <div className="app-questions--question--meta">
-            <span
-              className={classNames('app-questions--question--tag', `app-questions--question--tag_${question.level}`)}
-            >
-              {question.level}
-            </span>
-            <time
-              dateTime={question.acceptedAt}
-              className="app-questions--question--date app-questions--question--date_long"
-            >
-              {longDate(question.acceptedAt)}
-            </time>
-            <time
-              dateTime={question.acceptedAt}
-              className="app-questions--question--date app-questions--question--date_short"
-            >
-              {shortDate(question.acceptedAt)}
-            </time>
-          </div>
-
-          {this.props.removable && (
-            <div className="app-questions--question--remove-container">
-              <button
-                className="app-questions--question--remove--icon"
-                onClick={() => this.props.toggleQuestion(question.id)}
-              />
-            </div>
-          )}
+          {this.renderMeta()}
+          {this.maybeRenderDeleteButton()}
         </div>
       </article>
+    );
+  }
+
+  maybeRenderCheckbox() {
+    if (!this.props.selectable) {
+      return null;
+    }
+
+    const { question } = this.props;
+    const isSelected = this.isCurrentQuestionSelected();
+
+    return (
+      <input
+        onChange={() => this.props.toggleQuestion(question.id)}
+        checked={isSelected}
+        type="checkbox"
+        className="app-questions--question--checkbox"
+      />
+    );
+  }
+
+  renderMeta() {
+    const { question } = this.props;
+
+    return (
+      <div className="app-questions--question--meta">
+        <span className={classNames('app-questions--question--tag', `app-questions--question--tag_${question.level}`)}>
+          {question.level}
+        </span>
+        <time
+          dateTime={question.acceptedAt}
+          className="app-questions--question--date app-questions--question--date_long"
+        >
+          {longDate(question.acceptedAt)}
+        </time>
+        <time
+          dateTime={question.acceptedAt}
+          className="app-questions--question--date app-questions--question--date_short"
+        >
+          {shortDate(question.acceptedAt)}
+        </time>
+      </div>
+    );
+  }
+
+  maybeRenderDeleteButton() {
+    if (!this.props.removable) {
+      return null;
+    }
+
+    const { question } = this.props;
+
+    return (
+      <div className="app-questions--question--remove-container">
+        <button
+          className="app-questions--question--remove--icon"
+          onClick={() => this.props.toggleQuestion(question.id)}
+        />
+      </div>
     );
   }
 
   private isCurrentQuestionSelected = () => {
     return isQuestionSelected(this.props.selectedQuestionIds, this.props.question.id);
   };
-
-  editQuestion = (_question: Question) => {};
-  deleteQuestionForever = (_question: Question) => {};
 }
