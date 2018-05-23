7a1bf825fde49fb91d3f55ad886dba6a1464e8d4
Michal Miszczyszyn <michal@mmiszy.pl>
2018-05-23 11:06:17 +0200

    Move to TS for the server

--- routes.d.ts
+++
@@ -1,8 +0,0 @@
-import { Registry, LinkProps } from 'next-routes';
-import { ReactNode } from 'react';
-export const Link: Registry['Link'];
-// @ts-ignore
-export interface LinkProps extends LinkProps {
-  children: ReactNode;
-}
-export const Router: Registry['Router'];
