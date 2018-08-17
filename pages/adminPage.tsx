235df22f49b12c248978340c766ac9b375fff3d8
Michal Miszczyszyn <michal@mmiszy.pl>
2018-08-18 01:36:56 +0200

    Fix login/auth

--- pages/adminPage.tsx
+++ pages/adminPage.tsx
@@ -2,6 +2,9 @@ import * as React from 'react';
 import { GetInitialPropsContext } from '../utils/types';
 import { redirect } from '../utils/redirect';
 import Layout from '../components/layout/Layout';
+import QuestionsListLayout from '../components/questions/questionsListLayout/QuestionsListLayout';
+import QuestionsSidebar from '../components/questions/questionsSidebar/QuestionsSidebar';
+import AllQuestions from '../components/questions/allQuestions/AllQuestions';
 
 export default class AdminPage extends React.Component {
   static async getInitialProps(ctx: GetInitialPropsContext) {
@@ -14,7 +17,12 @@ export default class AdminPage extends React.Component {
   render() {
     return (
       <Layout title="Admin">
-        <div className="container">aaa</div>
+        <QuestionsListLayout>
+          <div className="questions-container">
+            <QuestionsSidebar />
+            <AllQuestions />
+          </div>
+        </QuestionsListLayout>
       </Layout>
     );
   }
