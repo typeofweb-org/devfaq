891b6bdd31b50f5e061a80cd3528caeac851e4a8
Michal Miszczyszyn <michal@mmiszy.pl>
2018-10-03 21:16:06 +0200

    Update build scripts

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+DIR=`dirname $0`
+
+
+if [[ "$1" == "production" ]]; then
+  SUBDOMAIN="app"
+elif [[ "$1" == "staging" ]]; then
+  SUBDOMAIN="staging"
+else
+  echo 'Incorrect environment. "production" or "staging" allowed.'
+  exit 1
+fi
+
+ENVIRONMENT=$1
+
+echo $ENVIRONMENT:`git rev-parse --abbrev-ref HEAD`:`git rev-parse HEAD` > .version
+echo "🥁 VERSION: " `cat .version`
+
+echo "👉 Building…"
+NODE_ENV=$ENVIRONMENT npm run build
+echo "👉 Cleaning"
+ssh sinpai@s18.mydevil.net SUBDOMAIN=$SUBDOMAIN 'bash -s' < $DIR/ssh-script-clean.sh
+echo "👉 Uploading…"
+rsync -avP -e ssh --exclude=node_modules --exclude=".git" --include="**/.*" ./ sinpai@s18.mydevil.net:/home/sinpai/domains/$SUBDOMAIN.fefaq.pl/public_nodejs/
+echo "👉 Installing…"
+ssh sinpai@s18.mydevil.net SUBDOMAIN=$SUBDOMAIN 'bash -s' < $DIR/ssh-script-deploy.sh
+echo "👉 Done! 😎"
--- ssh-script-clean.sh
+++ ssh-script-clean.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-cd /home/sinpai/domains/staging.fefaq.pl/public_nodejs
+cd /home/sinpai/domains/$SUBDOMAIN.fefaq.pl/public_nodejs
 echo "👉 Cleaning"
 rm -rf .next
 rm -rf public
