39f707b7e5d4317df53950c3e54a64d29e874fa8
Michal Miszczyszyn <michal@mmiszy.pl>
2019-04-28 23:19:21 +0200

    Rename

--- ssh-script-deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-cd /home/sinpai/domains/$SUBDOMAIN.fefaq.pl/public_nodejs
-npm i -f
-echo "👉 Restarting…"
-devil www restart $SUBDOMAIN.fefaq.pl
-echo "👉 Fetching…"
-curl -I https://$SUBDOMAIN.fefaq.pl/
