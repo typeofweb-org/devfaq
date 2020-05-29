918bd4543332a7d63ec742bb01d8e5b32d5a41a6
Michal Miszczyszyn <michal@mmiszy.pl>
2020-05-29 02:13:55 +0200

    Revert "Add deploy scripts"
    This reverts commit 4fcaf20336b2498d4a2a6de1743d5cd7f58e1a0e.

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,28 @@
 set -e
 
 DIR=`dirname $0`
-ssh typeofweb@s18.mydevil.net 'bash -s' < $DIR/ssh-script-deploy.sh $1
+
+
+if [[ "$1" == "production" ]]; then
+  SUBDOMAIN="app"
+elif [[ "$1" == "staging" ]]; then
+  SUBDOMAIN="staging"
+else
+  echo 'Incorrect environment. "production" or "staging" allowed.'
+  exit 1
+fi
+
+ENVIRONMENT=$1
+
+echo $ENVIRONMENT:`git rev-parse --abbrev-ref HEAD`:`git rev-parse HEAD` > .version
+echo "🥁 VERSION: " `cat .version`
+
+echo "👉 Building…"
+NODE_ENV=production ENV=$environment npm run build
+echo "👉 Cleaning"
+ssh typeofweb@s18.mydevil.net SUBDOMAIN=$SUBDOMAIN 'bash -s' < $DIR/ssh-script-clean.sh
+echo "👉 Uploading…"
+rsync -avP -e ssh --exclude=node_modules --exclude=".git" --include="**/.*" ./ typeofweb@s18.mydevil.net:/home/typeofweb/domains/$SUBDOMAIN.devfaq.pl/public_nodejs/
+echo "👉 Installing…"
+ssh typeofweb@s18.mydevil.net SUBDOMAIN=$SUBDOMAIN 'bash -s' < $DIR/ssh-script-deploy.sh
+echo "👉 Done! 😎"
