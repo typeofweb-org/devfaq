bb07944a0a9470a12516bf585f8000a05a54e8b4
Bartosz Dryl <drylbartosz@gmail.com>
2020-06-03 12:51:52 +0200

    Update components/layout/AppSpinner.tsx
    Co-authored-by: Michał Miszczyszyn <mmiszy@users.noreply.github.com>

--- components/layout/AppSpinner.tsx
+++ components/layout/AppSpinner.tsx
@@ -8,7 +8,7 @@ const SUSPENSE_TIME = 150;
 const AppSpinnerComponent: React.FC<ReturnType<typeof mapStateToProps>> = React.memo(
   ({ isLoading }) => {
     const [show, setShow] = useState(false);
-    let timerId = useRef<number | undefined>();
+    const timerId = useRef<number | undefined>();
 
     const startTimer = () => {
       if (isLoading) {
