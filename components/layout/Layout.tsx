0b29769a98089a3c6bd9fd1c9225ed629e8fd709
drillprops <drylbartosz@gmail.com>
2020-06-03 12:01:14 +0200

    Rewrite Layout to functional component

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,40 +1,32 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import React from 'react';
-import NavigationHeader from '../headers/navigationHeader/NavigationHeader';
-import styles from './layout.module.scss';
-import CtaHeader from '../headers/ctaHeader/CtaHeader';
-import AppFooter from '../footer/AppFooter';
-import AppSpinner from './AppSpinner';
 import env from '../../utils/env';
-import { withRouter, NextRouter } from 'next/router';
+import AppFooter from '../footer/AppFooter';
+import CtaHeader from '../headers/ctaHeader/CtaHeader';
+import NavigationHeader from '../headers/navigationHeader/NavigationHeader';
+import AppSpinner from './AppSpinner';
+import styles from './layout.module.scss';
 
 interface LayoutProps {
   title?: string;
   description?: string;
-  router: NextRouter;
 }
-
-class Layout extends React.Component<LayoutProps> {
-  static defaultProps = {
-    title: 'Front-end Frequently Asked Questions',
-    description:
-      'DevFAQ.pl — największa baza pytań z front-endu tworzona przez społeczność. DevFAQ.pl jest serwisem internetowym służącym do udostępniania i wymiany pytań rekrutacyjnych na stanowiska front-end developerów.',
-  };
-
-  render() {
-    const { title, description, children } = this.props;
+const Layout: React.FC<LayoutProps> = React.memo(
+  ({
+    title = 'Front-end Frequently Asked Questions',
+    description = 'DevFAQ.pl — największa baza pytań z front-endu tworzona przez społeczność. DevFAQ.pl jest serwisem internetowym służącym do udostępniania i wymiany pytań rekrutacyjnych na stanowiska front-end developerów.',
+    children,
+  }) => {
+    const router = useRouter();
     return (
-      <React.Fragment>
+      <>
         <Head>
           <title key="title">DevFAQ.pl • {title}</title>
           <meta key="description" name="description" content={description} />
           <meta property="og:title" itemProp="title name" content={`DevFAQ.pl • ${title}`} />
           <meta property="og:description" itemProp="description" content={description} />
-          <meta
-            property="og:url"
-            itemProp="url"
-            content={`${env.ABSOLUTE_URL}${this.props.router!.asPath}`}
-          />
+          <meta property="og:url" itemProp="url" content={`${env.ABSOLUTE_URL}${router.asPath}`} />
         </Head>
         <div className={styles.appRoot}>
           <AppSpinner />
@@ -43,9 +35,9 @@ class Layout extends React.Component<LayoutProps> {
           <div className={styles.appContent}>{children}</div>
           <AppFooter />
         </div>
-      </React.Fragment>
+      </>
     );
   }
-}
+);
 
-export default withRouter<LayoutProps>(Layout);
+export default Layout;
