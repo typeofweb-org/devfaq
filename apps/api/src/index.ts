9f2d6e0834de197b6978a6d13bacb96b5257ccfe
Michal Miszczyszyn <michal@mmiszy.pl>
2020-06-15 11:03:26 +0200

    Add loggings with Sematext

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,32 @@
+import Boom from '@hapi/boom';
 import * as Sentry from '@sentry/node';
 import dotenv from 'dotenv';
+import Winston from 'winston';
+import WinstonLogsene from 'winston-logsene';
 
 import { getConfig } from './config';
 import { initDb } from './db';
 import { getServerWithPlugins } from './server';
 import { handleException } from './utils/utils';
+
+if (getConfig('NODE_ENV') !== 'production') {
+  dotenv.config({ path: '.env.dev' });
+} else {
+  dotenv.config();
+}
+
+const spmAgent = require('spm-agent-nodejs');
+const logger = Winston.createLogger({
+  levels: Winston.config.npm.levels,
+  transports: [
+    new WinstonLogsene({
+      token: process.env.LOGS_TOKEN,
+      level: 'debug',
+      type: 'test_logs',
+      url: 'http://logsene-receiver.sematext.com/_bulk',
+    }),
+  ],
+});
 
 if (!getConfig('SENTRY_DSN')) {
   console.warn('SENTRY_DSN is missing. No errors will be reported!');
@@ -16,17 +38,31 @@ if (!getConfig('SENTRY_DSN')) {
   });
 }
 
-if (getConfig('NODE_ENV') !== 'production') {
-  dotenv.config({ path: '.env.dev' });
-} else {
-  dotenv.config();
-}
-
 // tslint:disable-next-line:no-floating-promises
 (async () => {
   try {
     await initDb();
     const devfaqServer = await getServerWithPlugins();
+
+    devfaqServer.events.on('response', ({ url, method, info, response }) => {
+      const responseTime =
+        (info.completed !== undefined ? info.completed : info.responded) - info.received;
+
+      if (Boom.isBoom(response)) {
+        return;
+      }
+
+      const contentLength = response.headers['content-length'];
+      const status = response.statusCode;
+
+      logger.info('response', {
+        responseTime: responseTime,
+        contentLength: contentLength,
+        method: method.toUpperCase(),
+        url: url.toString(),
+        status: status,
+      });
+    });
     await devfaqServer.start();
 
     console.info('Server running at:', devfaqServer.info.uri);
