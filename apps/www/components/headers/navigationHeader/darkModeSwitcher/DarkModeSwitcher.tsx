0524dfb23c3bb56b680040fc5c0690f3fc57b39d
drillprop <drylbartosz@gmail.com>
2020-10-06 18:31:34 +0200

    Implement switching modes

--- apps/www/components/headers/navigationHeader/darkModeSwitcher/DarkModeSwitcher.tsx
+++ apps/www/components/headers/navigationHeader/darkModeSwitcher/DarkModeSwitcher.tsx
@@ -1,13 +1,57 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import styles from './darkModeSwitcher.module.scss';
 
-const DarkModeSwitcher = () => {
+const getInitialColorMode = () => {
+  const mql = window.matchMedia('(prefers-color-scheme: dark)');
+  const hasMediaQueryPreference = typeof mql.matches === 'boolean';
+  if (hasMediaQueryPreference) {
+    return mql.matches ? 'dark' : 'light';
+  }
+
+  if (typeof window === 'undefined') {
+    return 'light';
+  }
+
+  const persistedColorPreference = window.localStorage.getItem('color-mode');
+
+  if (persistedColorPreference === 'dark' || persistedColorPreference === 'light') {
+    return persistedColorPreference;
+  }
+
+  return 'light';
+};
+
+const DarkModeSwitcher = React.memo(() => {
+  const [colorMode, setColorMode] = useState<'light' | 'dark' | undefined>();
+
+  useEffect(() => {
+    const html = document.querySelector('html');
+    if (colorMode === 'dark') html?.classList.add('theme-dark');
+    if (colorMode === 'light') html?.classList.remove('theme-dark');
+  }, [colorMode]);
+
+  useEffect(() => {
+    const mode = getInitialColorMode();
+    setColorMode(mode);
+  }, []);
+
+  const handleColorModeChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    const mode = e.target.checked ? 'dark' : 'light';
+    setColorMode(mode);
+    window.localStorage.setItem('color-mode', mode);
+  }, []);
+
   return (
     <label className={styles.switch}>
-      <input type="checkbox" />
+      <input
+        onChange={handleColorModeChange}
+        type="checkbox"
+        aria-label="Switch between Dark and Light mode"
+        checked={colorMode === 'dark'}
+      />
     </label>
   );
-};
+});
 
 export default DarkModeSwitcher;
