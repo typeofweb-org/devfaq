1b38b59f7fbec761f9319320d40bfbb037b24a54
Bartosz Dryl <drylbartosz@gmail.com>
2020-10-07 21:33:24 +0200

    Dark mode (#238)
    * Create DarkModeSwitcher
    
    * Update css color variables
    
    * Fix issue with not visible switcher on firefox
    
    * Add box-shadow related css variables
    
    * Update css variables in spinner and questionItem
    
    * Implement switching modes
    
    * Create css variables for dark theme
    
    * Implement dark mode in add question modal
    
    * Fix issue with switching mode when changing page
    
    * Fix DarkModeSwitcher
    
    Co-authored-by: drillprop <bartosz@DESKTOP-3CT5NSG.localdomain>

---
+++ apps/www/components/headers/navigationHeader/darkModeSwitcher/DarkModeSwitcher.tsx
@@ -0,0 +1,52 @@
+import React, { useCallback, useEffect, useState } from 'react';
+
+import styles from './darkModeSwitcher.module.scss';
+
+const getInitialColorMode = () => {
+  if (typeof window === 'undefined') {
+    return 'light';
+  }
+
+  const persistedColorPreference = window.localStorage.getItem('color-mode');
+
+  if (persistedColorPreference === 'dark' || persistedColorPreference === 'light') {
+    return persistedColorPreference;
+  }
+
+  const mql = window.matchMedia('(prefers-color-scheme: dark)');
+  const hasMediaQueryPreference = typeof mql.matches === 'boolean';
+  if (hasMediaQueryPreference) {
+    return mql.matches ? 'dark' : 'light';
+  }
+
+  return 'light';
+};
+
+const DarkModeSwitcher = React.memo(() => {
+  const [colorMode, setColorMode] = useState<'light' | 'dark'>(() => getInitialColorMode());
+
+  useEffect(() => {
+    const html = document.querySelector('html');
+    if (colorMode === 'dark') html?.classList.add('theme-dark');
+    if (colorMode === 'light') html?.classList.remove('theme-dark');
+  }, [colorMode]);
+
+  const handleColorModeChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    const mode = e.target.checked ? 'dark' : 'light';
+    setColorMode(mode);
+    window.localStorage.setItem('color-mode', mode);
+  }, []);
+
+  return (
+    <label className={styles.switch}>
+      <input
+        onChange={handleColorModeChange}
+        type="checkbox"
+        aria-label="Switch between Dark and Light mode"
+        checked={colorMode === 'dark'}
+      />
+    </label>
+  );
+});
+
+export default DarkModeSwitcher;
