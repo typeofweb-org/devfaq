7c67900cc4675db4d0012f749aa285df1fbf87a7
Michal Miszczyszyn <michal@mmiszy.pl>
2022-11-20 18:52:02 +0100

    Add debugging info

--- apps/app/app/page.tsx
+++ apps/app/app/page.tsx
@@ -4,6 +4,10 @@ export default function Page() {
 	return (
 		<div>
 			Siema <Button />
+			<ul style={{ fontFamily: "monospace" }}>
+				<li>NEXT_PUBLIC_API_URL: {process.env.NEXT_PUBLIC_API_URL}</li>
+				<li>NEXT_PUBLIC_APP_URL: {process.env.NEXT_PUBLIC_APP_URL}</li>
+			</ul>
 		</div>
 	);
 }
