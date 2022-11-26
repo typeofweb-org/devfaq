c5860c2d401eab28988ee49f54fb7dc3e2b270a9
Michal Miszczyszyn <michal@mmiszy.pl>
2022-11-26 19:02:28 +0100

    Update deployment api
    Test
    
    Fix
    
    Test
    
    Update fetch depth
    
    Test

--- apps/app/app/page.tsx
+++ apps/app/app/page.tsx
@@ -5,6 +5,7 @@ export default function Page() {
 		<div className="m-2 p-2">
 			Siema <Button />
 			<ul className="font-mono">
+				<li>Test</li>
 				<li>NEXT_PUBLIC_API_URL: {process.env.NEXT_PUBLIC_API_URL}</li>
 				<li>NEXT_PUBLIC_APP_URL: {process.env.NEXT_PUBLIC_APP_URL}</li>
 			</ul>
