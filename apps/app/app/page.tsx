c9ac5308f934c2382f3997bdd090ddb66458633f
Michal Miszczyszyn <michal@mmiszy.pl>
2022-11-26 17:29:01 +0100

    Add prettier tailwind plugin and downgrade next to get live reloading #358

--- apps/app/app/page.tsx
+++ apps/app/app/page.tsx
@@ -2,9 +2,9 @@ import { Button } from "ui";
 
 export default function Page() {
 	return (
-		<div>
+		<div className="m-2 p-2">
 			Siema <Button />
-			<ul style={{ fontFamily: "monospace" }}>
+			<ul className="font-mono">
 				<li>NEXT_PUBLIC_API_URL: {process.env.NEXT_PUBLIC_API_URL}</li>
 				<li>NEXT_PUBLIC_APP_URL: {process.env.NEXT_PUBLIC_APP_URL}</li>
 			</ul>
