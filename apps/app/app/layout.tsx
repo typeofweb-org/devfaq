557e920db478c66549a3decb7a337ec330e4702a
Adrian Polak <adrian.polak20@gmail.com>
2022-11-26 17:25:38 +0100

    feat: setup tailwind (#359)

--- apps/app/app/layout.tsx
+++ apps/app/app/layout.tsx
@@ -1,5 +1,7 @@
 import { AnalyticsWrapper } from "../components/analytics";
 
+import "./globals.css";
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="pl">
