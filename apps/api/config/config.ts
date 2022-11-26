320cc2b3b76bafe8c48d66cf0510ced01850ac37
Michal Miszczyszyn <michal@mmiszy.pl>
2022-11-26 16:09:30 +0100

    Fix versions env

--- apps/api/config/config.ts
+++ apps/api/config/config.ts
@@ -1,7 +1,12 @@
-export function getConfig(name: "ENV"): "production" | "staging" | "development" | "test";
-export function getConfig(name: "NODE_ENV"): "production" | "development";
 export function getConfig(name: "PORT"): number;
+export function getConfig(name: "NODE_ENV"): "production" | "development";
+export function getConfig(name: "ENV"): "production" | "staging" | "development" | "test";
+export function getConfig(name: "GITHUB_CLIENT_ID"): string;
+export function getConfig(name: "GITHUB_CLIENT_SECRET"): string;
+export function getConfig(name: "GIT_BRANCH"): string;
+export function getConfig(name: "GIT_COMMIT_HASH"): string;
 export function getConfig(name: "VERSION"): string;
+export function getConfig(name: "SENTRY_VERSION"): string;
 export function getConfig(name: string): string;
 export function getConfig(name: string): string | number {
 	const val = process.env[name];
@@ -16,10 +21,14 @@ export function getConfig(name: string): string | number {
 		case "GITHUB_CLIENT_ID":
 		case "GITHUB_CLIENT_SECRET":
 			return val || "";
+		case "GIT_BRANCH":
+			return val || "(unknown_branch)";
+		case "GIT_COMMIT_HASH":
+			return val || "(unknown_commit_hash)";
 		case "VERSION":
-			return process.env.VERSION || "dev";
+			return getConfig("ENV") + ":" + getConfig("GIT_BRANCH") + ":" + getConfig("GIT_COMMIT_HASH");
 		case "SENTRY_VERSION":
-			return getConfig("VERSION").split(":").pop() || "";
+			return getConfig("GIT_COMMIT_HASH") || "";
 	}
 
 	if (!val) {
