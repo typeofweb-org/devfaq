63e6be3d3f0834e7bd82f836d8fc447a897f750c
adipol1359 <adrian.polak20@gmail.com>
2022-12-03 19:28:51 +0100

    refactor(app): remove title attributes

--- apps/app/app/DefaultTags.tsx
+++ apps/app/app/DefaultTags.tsx
@@ -5,7 +5,7 @@ type DefaultTagsProps = Readonly<{
 
 export const DefaultTags = ({ title = "", description = "" }: DefaultTagsProps) => (
 	<>
-		<title key="title">{`DevFAQ.pl • ${title}`}</title>
+		<title>{`DevFAQ.pl • ${title}`}</title>
 		<meta key="description" name="description" content={description} />
 	</>
 );
