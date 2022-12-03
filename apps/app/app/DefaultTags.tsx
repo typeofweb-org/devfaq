d62dd841e754444c9733a48c441c8cf040bc6505
adipol1359 <adrian.polak20@gmail.com>
2022-12-03 20:02:47 +0100

    refactor(app): move common html tags into DefaultTags component

--- apps/app/app/DefaultTags.tsx
+++ apps/app/app/DefaultTags.tsx
@@ -7,5 +7,8 @@ export const DefaultTags = ({ title = "", description = "" }: DefaultTagsProps) 
 	<>
 		<title>{`DevFAQ.pl • ${title}`}</title>
 		<meta name="description" content={description} />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
 	</>
 );
--- apps/app/app/layout.tsx
+++ apps/app/app/layout.tsx
@@ -5,11 +5,6 @@ import "./globals.css";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="pl">
-			<head>
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-			</head>
 			<body>
 				{children}
 				<AnalyticsWrapper />
