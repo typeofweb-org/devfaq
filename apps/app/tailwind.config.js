6cf9c9ab14be2328c65af55659b6ada4bd4a28a5
xStrixU <strixu155@gmail.com>
2022-12-05 09:39:53 +0100

    chore(app): define violet color palette

--- apps/app/tailwind.config.js
+++ apps/app/tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
 	content: ["./src/**/*.{ts,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				violet: {
+					50: "#F1EDF9",
+					100: "#DACEEF",
+					200: "#D4C9E9",
+					300: "#C2ABEA",
+					400: "#A684E1",
+					500: "#8B5FD8",
+					600: "#713CCF",
+					700: "#673AB7",
+					800: "#401886",
+					900: "#3E2B5F",
+				},
+			},
+		},
 	},
 	plugins: [],
 };
