7b2c5cedf19e7d55f1ac70282cefccd69f670485
adipol1359 <adrian.polak20@gmail.com>
2022-12-05 14:06:34 +0100

    feat(app): add Link props

--- apps/app/src/components/ActiveLink.tsx
+++ apps/app/src/components/ActiveLink.tsx
@@ -3,20 +3,27 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { twMerge } from "tailwind-merge";
-import type { ReactNode } from "react";
+import type { ComponentProps, ReactNode } from "react";
 
 type ActiveLinkProps = Readonly<{
 	href: string;
 	activeClassName: string;
 	children: ReactNode;
-}>;
+}> &
+	ComponentProps<typeof Link>;
 
-export const ActiveLink = ({ href, activeClassName, children }: ActiveLinkProps) => {
+export const ActiveLink = ({
+	href,
+	className,
+	activeClassName,
+	children,
+	...rest
+}: ActiveLinkProps) => {
 	const pathname = usePathname();
 	const isActive = href === pathname;
 
 	return (
-		<Link href={href} className={twMerge(isActive && activeClassName)}>
+		<Link href={href} className={twMerge(className, isActive && activeClassName)} {...rest}>
 			{children}
 		</Link>
 	);
