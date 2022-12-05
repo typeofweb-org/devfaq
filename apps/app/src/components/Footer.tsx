f0cd1d0f19b3a08b8a7f1009b4677e9f1b2ce4f2
xStrixU <strixu155@gmail.com>
2022-12-05 16:48:16 +0100

    feat(app): add container to footer

--- apps/app/src/components/Footer.tsx
+++ apps/app/src/components/Footer.tsx
@@ -1,28 +1,20 @@
 import Link from "next/link";
-
-const LINK_CLASS_NAME = "hidden sm:inline";
+import { Container } from "./Container";
 
 export const Footer = () => (
-	<footer className="flex h-14 items-center justify-center bg-violet-700 dark:bg-violet-800 sm:justify-end">
-		<nav className="flex gap-7 text-sm text-white">
-			<Link href="/about" className={LINK_CLASS_NAME}>
-				Jak korzystać?
-			</Link>
-			<Link href="/regulations">Regulamin</Link>
-			<Link href="/authors" className={LINK_CLASS_NAME}>
-				Autorzy
-			</Link>
-			<a
-				href="https://www.facebook.com/DevFAQ"
-				target="_blank"
-				rel="noreferrer"
-				className={LINK_CLASS_NAME}
-			>
-				Facebook
-			</a>
-			<a href="https://typeofweb.com" target="_blank" rel="noreferrer">
-				Type of Web
-			</a>
-		</nav>
+	<footer className="bg-violet-700 dark:bg-violet-800">
+		<Container className="flex h-14 items-center justify-center sm:justify-end">
+			<nav className="flex flex-wrap justify-center gap-x-7 text-sm text-white">
+				<Link href="/about">Jak korzystać?</Link>
+				<Link href="/regulations">Regulamin</Link>
+				<Link href="/authors">Autorzy</Link>
+				<a href="https://www.facebook.com/DevFAQ" target="_blank" rel="noreferrer">
+					Facebook
+				</a>
+				<a href="https://typeofweb.com" target="_blank" rel="noreferrer">
+					Type of Web
+				</a>
+			</nav>
+		</Container>
 	</footer>
 );
