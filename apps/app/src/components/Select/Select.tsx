713071a07955b5c9b101482755a61cf8ceaf573c
adipol1359 <adrian.polak20@gmail.com>
2022-12-08 15:35:06 +0100

    feat(app): add appearance none

--- apps/app/src/components/Select/Select.tsx
+++ apps/app/src/components/Select/Select.tsx
@@ -4,7 +4,7 @@ import type { SelectHTMLAttributes } from "react";
 export const Select = ({ className, ...props }: SelectHTMLAttributes<HTMLSelectElement>) => (
 	<select
 		className={twMerge(
-			"cursor-pointer appearance-none border-b border-primary bg-select-purple bg-no-repeat py-2 pr-6 pl-1 text-sm capitalize text-primary transition-shadow duration-100 focus:shadow-[0_0_10px] focus:shadow-primary focus:outline-0",
+			"cursor-pointer appearance-none rounded-none border-b border-primary bg-select-purple bg-no-repeat py-2 pr-6 pl-1 text-sm capitalize text-primary transition-shadow duration-100 focus:shadow-[0_0_10px] focus:shadow-primary focus:outline-0",
 			className,
 		)}
 		{...props}
