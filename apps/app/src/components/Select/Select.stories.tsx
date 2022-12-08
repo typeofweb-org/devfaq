09e924f66230d1882bf6765a239f5f170534f8ca
adipol1359 <adrian.polak20@gmail.com>
2022-12-08 12:56:03 +0100

    feat(app): add select options to story

--- apps/app/src/components/Select/Select.stories.tsx
+++ apps/app/src/components/Select/Select.stories.tsx
@@ -4,6 +4,15 @@ import { Select } from "./Select";
 const meta: Meta<typeof Select> = {
 	title: "Select",
 	component: Select,
+	args: {
+		children: (
+			<>
+				<option>lorem</option>
+				<option>ipsum</option>
+				<option>dolor</option>
+			</>
+		),
+	},
 };
 
 export default meta;
