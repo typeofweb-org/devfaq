c0a366a1854220c93e4ae74a9d3f46ae3c3e581b
adipol1359 <adrian.polak20@gmail.com>
2022-12-09 08:43:16 +0100

    feat(app): add modal dark mode

--- apps/app/src/components/Select/Select.tsx
+++ apps/app/src/components/Select/Select.tsx
@@ -4,7 +4,7 @@ import type { SelectHTMLAttributes } from "react";
 export const Select = ({ className, ...props }: SelectHTMLAttributes<HTMLSelectElement>) => (
 	<select
 		className={twMerge(
-			"select-purple cursor-pointer appearance-none rounded-none border-b border-primary py-2 pr-6 pl-1 text-base capitalize text-primary transition-shadow duration-100 focus:shadow-[0_0_10px] focus:shadow-primary focus:outline-0",
+			"select-purple cursor-pointer appearance-none rounded-none border-b border-primary bg-transparent py-2 pr-6 pl-1 text-base capitalize text-primary transition-shadow duration-100 focus:shadow-[0_0_10px] focus:shadow-primary focus:outline-0 dark:border-neutral-200 dark:text-neutral-200 dark:focus:shadow-white",
 			className,
 		)}
 		{...props}
