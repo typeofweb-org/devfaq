04df6b3ad521136c69fcba3974874e064822d482
xStrixU <strixu155@gmail.com>
2022-12-09 14:21:59 +0100

    fix(app): fix z-index values

--- apps/app/src/components/CtaHeader/CtaHeader.tsx
+++ apps/app/src/components/CtaHeader/CtaHeader.tsx
@@ -17,7 +17,7 @@ const CtaHeaderActiveLink = (props: CtaHeaderActiveLinkProps) => (
 );
 
 export const CtaHeader = () => (
-	<div className="sticky top-0 bg-primary">
+	<div className="sticky top-0 z-20 bg-primary">
 		<Container as="header" className="flex h-14 items-center justify-between">
 			<nav className="flex grow gap-4 text-sm text-white sm:grow-0">
 				<CtaHeaderActiveLink href="/questions">Lista pytań</CtaHeaderActiveLink>
