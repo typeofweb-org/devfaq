a583cbc435bd9875ad2be5b3a3db0857a389b5af
adipol1359 <adrian.polak20@gmail.com>
2022-12-12 20:19:40 +0100

    feat(app): add dark mode to default select variant

--- apps/app/src/components/Select/Select.tsx
+++ apps/app/src/components/Select/Select.tsx
@@ -2,7 +2,8 @@ import { twMerge } from "tailwind-merge";
 import type { SelectHTMLAttributes } from "react";
 
 const variants = {
-	default: "select bg-white py-1 pl-1 pr-6 text-neutral-700",
+	default:
+		"select bg-white py-1 pl-1 pr-6 text-neutral-700 dark:bg-white-dark dark:text-neutral-200",
 	purple:
 		"select-purple border-b border-primary bg-transparent py-2 pr-6 pl-1 capitalize text-primary transition-shadow duration-100 focus:shadow-[0_0_10px] focus:shadow-primary dark:border-neutral-200 dark:text-neutral-200 dark:focus:shadow-white",
 };
