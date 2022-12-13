e4862a8ebb37830155eab341cb8b32fa1cdf9359
adipol1359 <adrian.polak20@gmail.com>
2022-12-13 14:55:43 +0100

    refactor(app): move logic into hook

--- apps/app/src/hooks/useDevFAQRouter.ts
+++ apps/app/src/hooks/useDevFAQRouter.ts
@@ -1,12 +1,17 @@
-import { useSearchParams } from "next/navigation";
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
 
 export const useDevFAQRouter = () => {
+	const router = useRouter();
 	const searchParams = useSearchParams();
+	const pathname = usePathname();
 
 	const mergeQueryParams = (data: Record<string, string>) => {
 		const params = { ...Object.fromEntries(searchParams.entries()), ...data };
+		const query = new URLSearchParams(params).toString();
 
-		return new URLSearchParams(params).toString();
+		if (pathname) {
+			router.push(`${pathname}?${query}`);
+		}
 	};
 
 	return { mergeQueryParams };
--- apps/app/src/hooks/useQuestionsOrderBy.ts
+++ apps/app/src/hooks/useQuestionsOrderBy.ts
@@ -1,18 +1,14 @@
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 import { useDevFAQRouter } from "./useDevFAQRouter";
 
 export const useQuestionsOrderBy = () => {
-	const pathname = usePathname();
-	const router = useRouter();
 	const searchParams = useSearchParams();
 	const { mergeQueryParams } = useDevFAQRouter();
 
 	const orderBy = searchParams.get("sortBy") || undefined;
 
 	const setOrderBy = (order: string) => {
-		if (pathname) {
-			router.push(`${pathname}?${mergeQueryParams({ sortBy: order })}`);
-		}
+		mergeQueryParams({ sortBy: order });
 	};
 
 	return { orderBy, setOrderBy };
