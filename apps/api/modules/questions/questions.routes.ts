2a7bcb597c78c34ff7b5e53ebd3ce29f60d34c07
xStrixU <strixu155@gmail.com>
2022-12-14 11:47:53 +0100

    feat(api): add endpoint to get votes

--- apps/api/modules/questions/questions.routes.ts
+++ apps/api/modules/questions/questions.routes.ts
@@ -10,6 +10,7 @@ import {
 	generateGetQuestionByIdSchema,
 	upvoteQuestionSchema,
 	downvoteQuestionSchema,
+	generateGetQuestionsVotesSchema,
 } from "./questions.schemas.js";
 
 const questionsPlugin: FastifyPluginAsync = async (fastify) => {
@@ -77,11 +78,6 @@ const questionsPlugin: FastifyPluginAsync = async (fastify) => {
 								QuestionVote: true,
 							},
 						},
-						QuestionVote: {
-							where: {
-								userId: request.session.data?._user.id || 0,
-							},
-						},
 					},
 				}),
 			]);
@@ -95,7 +91,6 @@ const questionsPlugin: FastifyPluginAsync = async (fastify) => {
 					_statusId: q.statusId,
 					acceptedAt: q.acceptedAt?.toISOString(),
 					votesCount: q._count.QuestionVote,
-					currentUserVotedOn: q.QuestionVote.length > 0,
 				};
 			});
 
@@ -126,9 +121,67 @@ const questionsPlugin: FastifyPluginAsync = async (fastify) => {
 				_levelId: newQuestion.levelId,
 				_statusId: newQuestion.statusId,
 				acceptedAt: newQuestion.acceptedAt?.toISOString(),
-				currentUserVotedOn: false,
 				votesCount: 0,
 			};
+
+			return { data };
+		},
+	});
+
+	fastify.withTypeProvider<TypeBoxTypeProvider>().route({
+		url: "/questions/votes",
+		method: "GET",
+		schema: generateGetQuestionsVotesSchema(args),
+		async handler(request, reply) {
+			const { category, level, status = "accepted", limit, offset, order, orderBy } = request.query;
+			const levels = level?.split(",");
+
+			const where = {
+				...(category && { categoryId: category }),
+				...(levels && { levelId: { in: levels } }),
+				...(status && request.session.data?._user._roleId === "admin"
+					? { statusId: status }
+					: { statusId: "accepted" }),
+			};
+
+			const questions = await fastify.db.question.findMany({
+				where,
+				take: limit,
+				skip: offset,
+				...(order &&
+					orderBy && {
+						orderBy: {
+							...(orderBy === "votesCount"
+								? {
+										QuestionVote: {
+											_count: order,
+										},
+								  }
+								: {
+										[orderBy]: order,
+								  }),
+						},
+					}),
+				select: {
+					id: true,
+					_count: {
+						select: {
+							QuestionVote: true,
+						},
+					},
+					QuestionVote: {
+						where: {
+							userId: request.session.data?._user.id || 0,
+						},
+					},
+				},
+			});
+
+			const data = questions.map((q) => ({
+				id: q.id,
+				votesCount: q._count.QuestionVote,
+				currentUserVotedOn: q.QuestionVote.length > 0,
+			}));
 
 			return { data };
 		},
@@ -163,11 +216,6 @@ const questionsPlugin: FastifyPluginAsync = async (fastify) => {
 							QuestionVote: true,
 						},
 					},
-					QuestionVote: {
-						where: {
-							userId: request.session.data?._user.id || 0,
-						},
-					},
 				},
 			});
 
@@ -179,7 +227,6 @@ const questionsPlugin: FastifyPluginAsync = async (fastify) => {
 				_statusId: q.statusId,
 				acceptedAt: q.acceptedAt?.toISOString(),
 				votesCount: q._count.QuestionVote,
-				currentUserVotedOn: q.QuestionVote.length > 0,
 			};
 
 			return { data };
@@ -210,11 +257,6 @@ const questionsPlugin: FastifyPluginAsync = async (fastify) => {
 							QuestionVote: true,
 						},
 					},
-					QuestionVote: {
-						where: {
-							userId: request.session.data?._user.id || 0,
-						},
-					},
 				},
 			});
 
@@ -230,7 +272,6 @@ const questionsPlugin: FastifyPluginAsync = async (fastify) => {
 				_statusId: q.statusId,
 				acceptedAt: q.acceptedAt?.toISOString(),
 				votesCount: q._count.QuestionVote,
-				currentUserVotedOn: q.QuestionVote.length > 0,
 			};
 
 			return { data };
