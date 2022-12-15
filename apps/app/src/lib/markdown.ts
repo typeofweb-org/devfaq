126f32b16fb4c09feaf60faf33b12968449a52bb
adipol1359 <adrian.polak20@gmail.com>
2022-12-15 14:13:24 +0100

    refactor(app): disable prism plugin

--- apps/app/src/lib/markdown.ts
+++ apps/app/src/lib/markdown.ts
@@ -1,9 +1,9 @@
-import remarkPrism from "remark-prism";
+// import remarkPrism from "remark-prism";
 import { serialize } from "next-mdx-remote/serialize";
 
 export const serializeSource = (source: string) =>
 	serialize(source, {
 		mdxOptions: {
-			remarkPlugins: [remarkPrism],
+			// remarkPlugins: [remarkPrism],
 		},
 	});
