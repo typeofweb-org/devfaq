c02948dae758d824317cae42fe07c8c727f4acfd
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-12-18 20:06:46 +0100

    Simplify types in AppTitle

--- apps/app/src/components/AppTitle.tsx
+++ apps/app/src/components/AppTitle.tsx
@@ -4,11 +4,9 @@ import Link from "next/link";
 import { useIsHome } from "../hooks/useIsHome";
 import { AppLogo } from "./AppLogo";
 
-type TitleTag = keyof Pick<JSX.IntrinsicElements, "h1" | "div">;
-
 export const AppTitle = () => {
 	const isHome = useIsHome();
-	const Tag: TitleTag = isHome ? "h1" : "div";
+	const Tag = isHome ? "h1" : "div";
 
 	return (
 		<Tag>
