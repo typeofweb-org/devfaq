0d63626b0b2d425b60c5414b87d879ed4a996a40
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-12-18 11:36:34 +0100

    fix(app): fix contrast ratio issues and more (#429)
    * fix(app): adjust contrast ratio of Select component
    
    * fix(app): adjust contrast ratio of Button component
    
    * feat(app): add a 'not-allowed' cursor style to disabled Button
    
    * Adjust CloseButton color on different themes
    
    * Adjust CloseButton color on different themes - standardize
    
    * Change color of Select caret when dark mode is enabled

--- apps/app/src/components/Select/Select.tsx
+++ apps/app/src/components/Select/Select.tsx
@@ -5,7 +5,7 @@ const variants = {
 	default:
 		"select bg-white py-1 pl-1 pr-6 text-neutral-700 dark:bg-white-dark dark:text-neutral-200",
 	purple:
-		"select-purple border-b border-primary bg-transparent py-2 pr-6 pl-1 capitalize text-primary transition-shadow duration-100 focus:shadow-[0_0_10px] focus:shadow-primary dark:border-neutral-200 dark:text-neutral-200 dark:focus:shadow-white",
+		"select-purple dark:select border-b border-primary bg-transparent py-2 pr-6 pl-1 capitalize text-primary transition-shadow duration-100 focus:shadow-[0_0_10px] focus:shadow-primary dark:border-neutral-200 dark:text-neutral-200 dark:focus:shadow-white dark:bg-white-dark",
 };
 
 type SelectProps = Readonly<{
