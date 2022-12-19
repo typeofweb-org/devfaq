00b4b36147a94d3b984073e18e61c4a88cc048fc
adipol1359 <adrian.polak20@gmail.com>
2022-12-19 20:19:39 +0100

    refactor(app): remove unused import

--- apps/app/src/components/AddQuestionModal/QuestionEditor/QuestionEditor.tsx
+++ apps/app/src/components/AddQuestionModal/QuestionEditor/QuestionEditor.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useRef, useState, Suspense } from "react";
-import { twMerge } from "tailwind-merge";
 import dynamic from "next/dynamic";
 import BoldIcon from "../../../../public/icons/toolbar-bold.svg";
 import ItalicIcon from "../../../../public/icons/toolbar-italic.svg";
--- apps/app/src/lib/markdown.ts
+++ apps/app/src/lib/markdown.ts
@@ -1,10 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import remarkStripHtml from "remark-strip-html";
 import rehypePrism from "rehype-prism-plus";
 import { serialize } from "next-mdx-remote/serialize";
 
 export const serializeSource = (source: string) =>
 	serialize(source, {
 		mdxOptions: {
-			remarkPlugins: [],
+			remarkPlugins: [remarkStripHtml],
 			rehypePlugins: [rehypePrism],
 		},
 	});
