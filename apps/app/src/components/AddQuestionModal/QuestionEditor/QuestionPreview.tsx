d731bd5226a988930c898fa8351ddd9a0d5e793e
adipol1359 <adrian.polak20@gmail.com>
2022-12-19 14:49:05 +0100

    feat(app): format question content

--- apps/app/src/components/AddQuestionModal/QuestionEditor/QuestionPreview.tsx
+++ apps/app/src/components/AddQuestionModal/QuestionEditor/QuestionPreview.tsx
@@ -1,9 +1,9 @@
-import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
 import { use } from "react";
 import { serializeSource } from "../../../lib/markdown";
+import { QuestionContent } from "../../QuestionsList/QuestionItem/QuestionContent";
 
 export const QuestionPreview = ({ content }: { readonly content: string }) => {
 	const source = use(serializeSource(content));
 
-	return <MDXRemote {...source} />;
+	return <QuestionContent source={source} />;
 };
