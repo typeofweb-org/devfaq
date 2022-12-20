ffd248485c1fc63f5b342c82e681e29a28f4cece
Adrian Polak <adrian.polak20@gmail.com>
2022-12-20 10:51:51 +0100

    fix(app): fix frontend bugs (#444)
    * fix(app): redirect to first page after changing the sorting
    
    * fix(app): fix double slash
    
    * Update apps/app/src/hooks/useDefaultQuestionsPathname.ts
    
    Co-authored-by: Michał Miszczyszyn <michal@mmiszy.pl>

--- apps/app/src/hooks/useQuestionsLevels.ts
+++ apps/app/src/hooks/useQuestionsLevels.ts
@@ -1,23 +1,25 @@
 import { useSearchParams } from "next/navigation";
 import { parseQueryLevels, Level } from "../lib/level";
 import { useDevFAQRouter } from "./useDevFAQRouter";
+import { useDefaultQuestionsPathname } from "./useDefaultQuestionsPathname";
 
 export const useQuestionsLevels = () => {
 	const searchParams = useSearchParams();
 	const { mergeQueryParams } = useDevFAQRouter();
+	const { defaultPathname } = useDefaultQuestionsPathname();
 
 	const queryLevel = searchParams.get("level");
 	const queryLevels = parseQueryLevels(queryLevel);
 
 	const addLevel = (level: Level) => {
-		mergeQueryParams({ level: [...(queryLevels || []), level].join(",") });
+		mergeQueryParams({ level: [...(queryLevels || []), level].join(",") }, defaultPathname);
 	};
 
 	const removeLevel = (level: Level) => {
 		if (queryLevels) {
 			const newQueryLevels = queryLevels.filter((l) => l !== level);
 
-			mergeQueryParams({ level: newQueryLevels.join(",") });
+			mergeQueryParams({ level: newQueryLevels.join(",") }, defaultPathname);
 		}
 	};
 
