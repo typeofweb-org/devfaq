8163a1339b00931bf548ee7d369d8a13339578d1
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-12-20 21:58:17 +0100

    Revert last change

--- apps/app/src/components/LoginForm/LoginForm.tsx
+++ apps/app/src/components/LoginForm/LoginForm.tsx
@@ -3,7 +3,7 @@ import { BackLink } from "./BackLink";
 import { GitHubLoginButton } from "./GitHubLoginButton";
 
 export const LoginForm = () => (
-	<div className="mx-auto flex max-w-[520px] flex-col items-center gap-10 p-8 pt-0 sm:pt-8">
+	<div className="mx-auto flex max-w-[520px] flex-col items-center gap-10 p-8">
 		<AppLogo />
 		<p className="text-center text-xl text-white">
 			Stwórz konto już dzisiaj i korzystaj z dodatkowych funkcji serwisu DevFAQ!
