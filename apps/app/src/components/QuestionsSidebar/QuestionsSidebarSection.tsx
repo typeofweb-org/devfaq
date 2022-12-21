2c9cf263c3b5b434a89471e055ce0f1d46990967
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-12-21 21:02:09 +0100

    fix(app): improve QuestionsSidebar styles

--- apps/app/src/components/QuestionsSidebar/QuestionsSidebarSection.tsx
+++ apps/app/src/components/QuestionsSidebar/QuestionsSidebarSection.tsx
@@ -6,7 +6,7 @@ type QuestionsSidebarSection = Readonly<{
 }>;
 
 export const QuestionsSidebarSection = ({ title, children }: QuestionsSidebarSection) => (
-	<section className="mt-6">
+	<section>
 		<h2 className="mb-6 border-b border-violet-700 pb-2 text-sm text-violet-700 dark:border-neutral-200 dark:text-neutral-200">
 			{title}
 		</h2>
