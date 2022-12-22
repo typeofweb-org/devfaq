a2b3b6c6180d89ce258b1e804db28f58dc82059d
adipol1359 <adrian.polak20@gmail.com>
2022-12-22 11:41:18 +0100

    fix(app): fix frontend bugs

--- apps/app/src/components/QuestionItem/QuestionContent.tsx
+++ apps/app/src/components/QuestionItem/QuestionContent.tsx
@@ -2,19 +2,16 @@
 
 import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
 
-import "prismjs";
-
-// import "prismjs/themes/prism-coy.css";
 import "../../styles/prism-one-light.css";
 
 type QuestionContentProps = Readonly<{
-	source: MDXRemoteSerializeResult;
+	source: MDXRemoteSerializeResult | null;
 }>;
 
 export const QuestionContent = ({ source }: QuestionContentProps) => {
 	return (
 		<div className="question-content prose prose-sm min-w-0 max-w-full grow px-2 prose-code:px-0 prose-pre:bg-transparent prose-pre:px-0 prose-pre:text-base dark:prose-invert md:prose-base md:prose-pre:text-base">
-			<MDXRemote {...source} />
+			{source && <MDXRemote {...source} />}
 		</div>
 	);
 };
