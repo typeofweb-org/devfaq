8415f1f0956d6853ee9ffa05388da83795dd59d9
adipol1359 <adrian.polak20@gmail.com>
2022-12-22 11:42:14 +0100

    refactor(app): remove null

--- apps/app/src/components/QuestionItem/QuestionContent.tsx
+++ apps/app/src/components/QuestionItem/QuestionContent.tsx
@@ -5,13 +5,13 @@ import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
 import "../../styles/prism-one-light.css";
 
 type QuestionContentProps = Readonly<{
-	source: MDXRemoteSerializeResult | null;
+	source: MDXRemoteSerializeResult;
 }>;
 
 export const QuestionContent = ({ source }: QuestionContentProps) => {
 	return (
 		<div className="question-content prose prose-sm min-w-0 max-w-full grow px-2 prose-code:px-0 prose-pre:bg-transparent prose-pre:px-0 prose-pre:text-base dark:prose-invert md:prose-base md:prose-pre:text-base">
-			{source && <MDXRemote {...source} />}
+			<MDXRemote {...source} />
 		</div>
 	);
 };
