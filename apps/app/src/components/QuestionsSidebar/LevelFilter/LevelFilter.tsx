6dc473157b5476e773b1d7925c26fa1f04a7eb70
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-12-24 12:40:43 +0100

    Remove redundant code and comments

--- apps/app/src/components/QuestionsSidebar/LevelFilter/LevelFilter.tsx
+++ apps/app/src/components/QuestionsSidebar/LevelFilter/LevelFilter.tsx
@@ -7,7 +7,6 @@ import { LevelFilterItem } from "./LevelFilterItem";
 export const LevelFilter = () => (
 	<QuestionsSidebarSection title="Wybierz poziom">
 		<ul className="flex justify-center gap-x-4 overflow-x-auto pb-4 sm:flex-col sm:flex-wrap sm:justify-between sm:gap-x-0 sm:gap-y-3 sm:overflow-x-visible sm:p-0 small-filters:flex-row small-filters:gap-y-4">
-			{/* <ul className="flex flex-wrap justify-center gap-3 sm:flex-col small-filters:flex-row"> */}
 			{levels.map((level) => (
 				<li key={level} className="pt-1">
 					<LevelFilterItem variant={level}>{level}</LevelFilterItem>
