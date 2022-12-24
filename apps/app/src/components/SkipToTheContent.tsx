2fad6c07edb4ab7fe4c26d9d00247f6717732218
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-12-24 19:40:21 +0100

    refactor(app): remove redundant styles

--- apps/app/src/components/SkipToTheContent.tsx
+++ apps/app/src/components/SkipToTheContent.tsx
@@ -1,7 +1,7 @@
 export const SkipToTheContent = () => (
 	<a
 		href={`#main-content`}
-		className="fixed -top-[100%] z-50 m-4 border-white bg-violet-900 p-4 px-8 text-white focus:visible focus:top-0 focus:border-2 focus:border-dashed"
+		className="fixed -top-[100%] z-50 m-4 border-white bg-violet-900 p-4 px-8 text-white focus:top-0 focus:border-2 focus:border-dashed"
 	>
 		Przejdź do treści
 	</a>
