b8b209d811d860fe14f62d16bdc007c89ad1d32e
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-12-25 15:11:29 +0100

    refactor(app): use Button component instead of native html tag in MobileActionsButton

--- apps/app/src/components/MobileActionButtons/MobileActionButton.tsx
+++ apps/app/src/components/MobileActionButtons/MobileActionButton.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import { Button } from "../Button/Button";
 
 type MobileActionButtonProps = Readonly<{
 	children: ReactNode;
@@ -7,8 +8,9 @@ type MobileActionButtonProps = Readonly<{
 }>;
 
 export const MobileActionButton = (props: MobileActionButtonProps) => (
-	<button
-		className="flex h-11 w-11 appearance-none items-center justify-center rounded-full bg-primary shadow-md shadow-neutral-600 dark:shadow-neutral-900"
+	<Button
+		variant="brandingInverse"
 		{...props}
+		className="flex h-11 w-11 items-center justify-center rounded-full border-none p-0 shadow-md shadow-neutral-600 dark:shadow-neutral-900"
 	/>
 );
