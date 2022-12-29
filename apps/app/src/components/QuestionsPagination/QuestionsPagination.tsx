e71139be31640e5fdfd5ff63260b0a8c89185be1
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-12-29 21:35:23 +0100

    fix(app): improve aria-label attribute in pagination"

--- apps/app/src/components/QuestionsPagination/QuestionsPagination.tsx
+++ apps/app/src/components/QuestionsPagination/QuestionsPagination.tsx
@@ -46,7 +46,9 @@ export const QuestionsPagination = ({ current, total, getHref }: QuestionsPagina
 							href={getHref(i)}
 							className="flex h-7 w-7 cursor-pointer items-center justify-center rounded-full border-2 border-primary text-primary transition-colors duration-300 hover:bg-violet-100 dark:text-white dark:hover:bg-violet-800"
 							activeClassName="bg-primary text-white hover:bg-primary"
-							aria-label={`Strona ${i}`}
+							aria-label={`Strona ${i}${
+								current - 1 === i ? ", poprzednia" : current + 1 == i ? ", kolejna" : ""
+							}`}
 							activeAttributes={{
 								"aria-current": "page",
 								"aria-label": `Strona ${i}, bieżąca`,
