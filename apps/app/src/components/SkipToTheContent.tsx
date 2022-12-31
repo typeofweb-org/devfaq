1aba77b3541326036ae7b4379e6b17618723ec72
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-12-31 13:58:17 +0100

    Update apps/app/src/components/SkipToTheContent.tsx
    Co-authored-by: Michał Miszczyszyn <michal@mmiszy.pl>

--- apps/app/src/components/SkipToTheContent.tsx
+++ apps/app/src/components/SkipToTheContent.tsx
@@ -1,8 +1,11 @@
-export const SkipToTheContent = () => (
-	<a
-		href={`#main-content`}
-		className="fixed -top-[100%] z-50 m-4 border-white bg-violet-900 p-4 px-8 text-white focus:top-0 focus:border-2 focus:border-dashed"
-	>
-		Przejdź do treści
-	</a>
-);
+export const SkipToTheContent = () => {
+	// @todo replace with `Link` when https://github.com/vercel/next.js/issues/44295 is fixed
+	return (
+		<a
+			href={`#main-content`}
+			className="fixed -top-[100%] z-50 m-4 border-white bg-violet-900 p-4 px-8 text-white focus:top-0 focus:border-2 focus:border-dashed"
+		>
+			Przejdź do treści
+		</a>
+	);
+};
