22310d28a686cf9d726401bc64592dfc759c3376
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-12-31 19:32:45 +0100

    Restore code which prevent layout shift after hide scrollbar

--- apps/app/src/utils/pageScroll.ts
+++ apps/app/src/utils/pageScroll.ts
@@ -1,11 +1,13 @@
 import "client-only";
 
-const classes = ["overflow-hidden", "sm:overflow-auto"];
+const classes = ["overflow-hidden"];
 
 export const lockScroll = () => {
+	document.body.style.paddingRight = `${window.innerWidth - document.body.offsetWidth}px`;
 	document.body.classList.add(...classes);
 };
 
 export const unlockScroll = () => {
+	document.body.style.paddingRight = "";
 	document.body.classList.remove(...classes);
 };
