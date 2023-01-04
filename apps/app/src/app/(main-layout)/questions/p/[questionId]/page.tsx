ff9047b622d7d6af2218b4215d1d8f9efd3088d5
Michal Miszczyszyn <michal@mmiszy.pl>
2023-01-04 17:30:37 +0100

    chore: hide answers

--- apps/app/src/app/(main-layout)/questions/p/[questionId]/page.tsx
+++ apps/app/src/app/(main-layout)/questions/p/[questionId]/page.tsx
@@ -1,9 +1,7 @@
 import { redirect } from "next/navigation";
-import { serializeSource } from "../../../../../lib/markdown";
-import { QuestionAnswers } from "../../../../../components/QuestionAnswers/QuestionAnswers";
 import { SingleQuestion } from "../../../../../components/SingleQuestion";
 import { serializeQuestionToMarkdown } from "../../../../../lib/question";
-import { getQuestionAnswers, getQuestionById } from "../../../../../services/questions.service";
+import { getQuestionById } from "../../../../../services/questions.service";
 import { Params } from "../../../../../types";
 
 export default async function SingleQuestionPage({ params }: { params: Params<"questionId"> }) {
@@ -13,25 +11,17 @@ export default async function SingleQuestionPage({ params }: { params: Params<"q
 		return redirect("/");
 	}
 
-	const [questionData, answersData] = await Promise.all([
+	const [questionData] = await Promise.all([
 		getQuestionById({
 			id: questionId,
 		}),
-		getQuestionAnswers({ id: questionId }),
 	]);
 
 	const question = await serializeQuestionToMarkdown(questionData.data.data);
-	const answers = await Promise.all(
-		answersData.data.data.map(async ({ content, ...rest }) => {
-			const mdxContent = await serializeSource(content);
-			return { mdxContent, ...rest };
-		}),
-	);
 
 	return (
 		<>
 			<SingleQuestion question={question} />
-			<QuestionAnswers questionId={questionId} answers={answers} />
 		</>
 	);
 }
