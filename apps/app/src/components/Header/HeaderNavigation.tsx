8bfb345705d36ab35062685d5a9f9067832b4315
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-01-05 00:16:15 +0100

    refactor(app): change hook name

--- apps/app/src/components/Header/HeaderNavigation.tsx
+++ apps/app/src/components/Header/HeaderNavigation.tsx
@@ -5,37 +5,40 @@ import type { ReactNode, MouseEvent } from "react";
 import { useEffect } from "react";
 import FocusLock from "react-focus-lock";
 import { lockScroll, unlockScroll } from "../../utils/pageScroll";
-import { useMobleMenu } from "../../hooks/useMobileMenu";
+import { useCloseOnBreakpoint } from "../../hooks/useCloseOnBreakpoint";
 import { ActiveNavigationLink } from "./ActiveNagivationLink";
 import { LoginNavigationLink } from "./LoginNavigationLink";
 
 const itemStyles = "ease h-0.5 w-6 bg-white transition duration-300";
 
 export const HeaderNavigation = ({ children }: { children: ReactNode }) => {
-	const { isOpen, setIsOpen } = useMobleMenu({ initialMenuOpen: false, maxMobileWidth: 640 });
+	const [isMobileMenuOpen, setIsMobileMenuOpen] = useCloseOnBreakpoint({
+		initialState: false,
+		breakpoint: 640,
+	});
 
 	const handleButtonClick = (event: MouseEvent<HTMLButtonElement>) => {
 		event.preventDefault();
-		isOpen ? unlockScroll() : lockScroll();
-		setIsOpen((prev) => !prev);
+		isMobileMenuOpen ? unlockScroll() : lockScroll();
+		setIsMobileMenuOpen((prev) => !prev);
 	};
 
 	const handleClickLink = () => {
-		setIsOpen(false);
+		setIsMobileMenuOpen(false);
 		unlockScroll();
 	};
 
 	useEffect(() => {
-		isOpen ? lockScroll() : unlockScroll();
-	}, [isOpen]);
+		isMobileMenuOpen ? lockScroll() : unlockScroll();
+	}, [isMobileMenuOpen]);
 
 	return (
-		<FocusLock disabled={!isOpen} className={twMerge("flex items-center")}>
+		<FocusLock disabled={!isMobileMenuOpen} className={twMerge("flex items-center")}>
 			<nav
 				id="header-navigation"
 				className={twMerge(
 					"fixed inset-0 z-30 flex flex-col items-center overflow-y-auto bg-primary py-20 text-xl uppercase sm:relative sm:flex sm:flex-row sm:items-center sm:gap-5 sm:py-0 sm:text-sm",
-					isOpen ? "flex" : "hidden",
+					isMobileMenuOpen ? "flex" : "hidden",
 				)}
 			>
 				<div className="mt-auto mb-auto flex flex-col items-center gap-10 sm:flex-row sm:gap-5">
@@ -73,17 +76,17 @@ export const HeaderNavigation = ({ children }: { children: ReactNode }) => {
 			<button
 				className={twMerge(
 					"right-4 z-40 flex h-8 w-8 flex-col items-center justify-center gap-1.5 sm:hidden",
-					isOpen ? "fixed" : "absolute",
+					isMobileMenuOpen ? "fixed" : "absolute",
 				)}
 				onClick={handleButtonClick}
 				type="button"
-				aria-label={`${isOpen ? "Zamknij" : "Otwórz"} menu`}
-				aria-expanded={isOpen}
+				aria-label={`${isMobileMenuOpen ? "Zamknij" : "Otwórz"} menu`}
+				aria-expanded={isMobileMenuOpen}
 				aria-controls="header-navigation"
 			>
-				<div className={twMerge(itemStyles, isOpen && "translate-y-2 rotate-45")} />
-				<div className={twMerge(itemStyles, isOpen ? "opacity-0" : "opacity-100")} />
-				<div className={twMerge(itemStyles, isOpen && "-translate-y-2 -rotate-45")} />
+				<div className={twMerge(itemStyles, isMobileMenuOpen && "translate-y-2 rotate-45")} />
+				<div className={twMerge(itemStyles, isMobileMenuOpen ? "opacity-0" : "opacity-100")} />
+				<div className={twMerge(itemStyles, isMobileMenuOpen && "-translate-y-2 -rotate-45")} />
 			</button>
 		</FocusLock>
 	);
---
+++ apps/app/src/hooks/useCloseOnBreakpoint.ts
@@ -0,0 +1,28 @@
+import { Dispatch, SetStateAction, useCallback, useEffect, useState } from "react";
+
+type UseCloseOnBreakpointProps = {
+	initialState: boolean;
+	breakpoint: number;
+};
+
+export const useCloseOnBreakpoint = ({
+	initialState,
+	breakpoint,
+}: UseCloseOnBreakpointProps): [boolean, Dispatch<SetStateAction<boolean>>] => {
+	const [isOpen, setIsOpen] = useState(initialState);
+
+	const handleResize = useCallback(() => {
+		if (window.innerWidth > breakpoint) {
+			setIsOpen(false);
+		}
+	}, [breakpoint]);
+
+	useEffect(() => {
+		if (isOpen) {
+			window.addEventListener("resize", handleResize);
+		}
+		return () => window.removeEventListener("resize", handleResize);
+	}, [handleResize, isOpen]);
+
+	return [isOpen, setIsOpen];
+};
