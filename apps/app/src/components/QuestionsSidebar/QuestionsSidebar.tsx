0c8211f284dfdc4c2399cf9f51d76dd1f8eb3d55
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-01-05 00:28:43 +0100

    feat(app): trap focus in QuestionsSidebar on mobile when is open

--- apps/app/src/components/QuestionsSidebar/QuestionsSidebar.tsx
+++ apps/app/src/components/QuestionsSidebar/QuestionsSidebar.tsx
@@ -2,15 +2,35 @@
 
 import { useEffect } from "react";
 import { twMerge } from "tailwind-merge";
+import FocusLock from "react-focus-lock";
 import { useUIContext } from "../../providers/UIProvider";
 import { Button } from "../Button/Button";
 import { CloseButton } from "../CloseButton/CloseButton";
 import { lockScroll, unlockScroll } from "../../utils/pageScroll";
+import { useCloseOnBreakpoint } from "../../hooks/useCloseOnBreakpoint";
 import { LevelFilter } from "./LevelFilter/LevelFilter";
 import { TechnologyFilter } from "./TechnologyFilter/TechnologyFilter";
 
 export const QuestionsSidebar = () => {
 	const { isSidebarOpen, closeSidebar } = useUIContext();
+	const [trapFocus, setTrapFocus] = useCloseOnBreakpoint({
+		initialState: isSidebarOpen,
+		breakpoint: 640,
+	});
+
+	useEffect(() => {
+		if (isSidebarOpen) {
+			setTrapFocus(true);
+		} else {
+			setTrapFocus(false);
+		}
+	}, [isSidebarOpen, setTrapFocus]);
+
+	useEffect(() => {
+		if (!trapFocus) {
+			closeSidebar();
+		}
+	}, [closeSidebar, trapFocus]);
 
 	useEffect(() => {
 		if (isSidebarOpen) {
@@ -21,18 +41,20 @@ export const QuestionsSidebar = () => {
 	}, [isSidebarOpen]);
 
 	return (
-		<aside
-			className={twMerge(
-				"border-aside fixed inset-0 z-50 flex shrink-0 -translate-x-full transform-gpu flex-col overflow-y-auto bg-gray-50 px-2.5 py-6 pb-4 transition-transform duration-200 dark:bg-neutral-800 sm:sticky sm:top-[56px] sm:z-auto sm:h-[calc(100vh-56px)] sm:w-62 sm:translate-x-0 sm:border-r-8 sm:pl-0 sm:pr-10 sm:transition-none sm:after:inline",
-				isSidebarOpen && "translate-x-0",
-			)}
-		>
-			<TechnologyFilter />
-			<LevelFilter />
-			<Button variant="brandingInverse" className="mt-auto sm:hidden" onClick={closeSidebar}>
-				Pokaż wyniki
-			</Button>
-			<CloseButton className="absolute top-1 right-1 sm:hidden" onClick={closeSidebar} />
-		</aside>
+		<FocusLock disabled={!trapFocus}>
+			<aside
+				className={twMerge(
+					"border-aside fixed inset-0 z-50 flex shrink-0 -translate-x-full transform-gpu flex-col overflow-y-auto bg-gray-50 px-2.5 py-6 pb-4 transition-transform duration-200 dark:bg-neutral-800 sm:sticky sm:top-[56px] sm:z-auto sm:h-[calc(100vh-56px)] sm:w-62 sm:translate-x-0 sm:border-r-8 sm:pl-0 sm:pr-10 sm:transition-none sm:after:inline",
+					isSidebarOpen && "translate-x-0",
+				)}
+			>
+				<TechnologyFilter />
+				<LevelFilter />
+				<Button variant="brandingInverse" className="mt-auto sm:hidden" onClick={closeSidebar}>
+					Pokaż wyniki
+				</Button>
+				<CloseButton className="absolute top-1 right-1 sm:hidden" onClick={closeSidebar} />
+			</aside>
+		</FocusLock>
 	);
 };
