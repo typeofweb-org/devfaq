df9e37f9d308d07b96c9e3e6ff39144e7062c28c
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-01-06 15:23:04 +0100

    feat(app): add focus lock in HeaderNavigation

--- apps/app/src/components/Header/HeaderNavigation.tsx
+++ apps/app/src/components/Header/HeaderNavigation.tsx
@@ -3,33 +3,38 @@
 import { useState } from "react";
 import { twMerge } from "tailwind-merge";
 import type { ReactNode, MouseEvent } from "react";
+import ReactFocusLock from "react-focus-lock";
 import { lockScroll, unlockScroll } from "../../utils/pageScroll";
+import { useIsAboveBreakpoint } from "../../hooks/useIsAboveBreakpoint";
 import { ActiveNavigationLink } from "./ActiveNagivationLink";
 import { LoginNavigationLink } from "./LoginNavigationLink";
 
 const itemStyles = "ease h-0.5 w-6 bg-white transition duration-300";
 
 export const HeaderNavigation = ({ children }: { children: ReactNode }) => {
-	const [isOpen, setIsOpen] = useState(false);
+	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+	const isAboveBreakpoint = useIsAboveBreakpoint({ breakpoint: 640 });
 
 	const handleButtonClick = (event: MouseEvent<HTMLButtonElement>) => {
 		event.preventDefault();
-		isOpen ? unlockScroll({ mobileOnly: true }) : lockScroll({ mobileOnly: true });
-		setIsOpen((prev) => !prev);
+		isMobileMenuOpen
+			? unlockScroll({ mobileOnly: true, preventLayoutShift: false })
+			: lockScroll({ mobileOnly: true, preventLayoutShift: false });
+		setIsMobileMenuOpen((prev) => !prev);
 	};
 
 	const handleClickLink = () => {
-		setIsOpen(false);
-		unlockScroll({ mobileOnly: true });
+		setIsMobileMenuOpen(false);
+		unlockScroll({ mobileOnly: true, preventLayoutShift: false });
 	};
 
 	return (
-		<>
+		<ReactFocusLock disabled={!isMobileMenuOpen || isAboveBreakpoint}>
 			<nav
 				id="header-navigation"
 				className={twMerge(
 					"fixed inset-0 z-30 flex flex-col items-center overflow-y-auto bg-primary py-20 text-xl uppercase sm:relative sm:flex sm:flex-row sm:items-center sm:gap-5 sm:py-0 sm:text-sm",
-					isOpen ? "flex" : "hidden",
+					isMobileMenuOpen ? "flex" : "hidden",
 				)}
 			>
 				<div className="mt-auto mb-auto flex flex-col items-center gap-10 sm:flex-row sm:gap-5">
@@ -67,18 +72,33 @@ export const HeaderNavigation = ({ children }: { children: ReactNode }) => {
 			<button
 				className={twMerge(
 					"right-4 z-40 flex h-8 w-8 flex-col items-center justify-center gap-1.5 sm:hidden",
-					isOpen ? "fixed" : "absolute",
+					isMobileMenuOpen ? "fixed" : "absolute",
 				)}
 				onClick={handleButtonClick}
 				type="button"
-				aria-label={`${isOpen ? "Zamknij" : "Otwórz"} menu`}
-				aria-expanded={isOpen}
+				aria-label={`${isMobileMenuOpen && !isAboveBreakpoint ? "Zamknij" : "Otwórz"} menu`}
+				aria-expanded={isMobileMenuOpen && !isAboveBreakpoint}
 				aria-controls="header-navigation"
 			>
-				<div className={twMerge(itemStyles, isOpen && "translate-y-2 rotate-45")} />
-				<div className={twMerge(itemStyles, isOpen ? "opacity-0" : "opacity-100")} />
-				<div className={twMerge(itemStyles, isOpen && "-translate-y-2 -rotate-45")} />
+				<div
+					className={twMerge(
+						itemStyles,
+						isMobileMenuOpen && !isAboveBreakpoint && "translate-y-2 rotate-45",
+					)}
+				/>
+				<div
+					className={twMerge(
+						itemStyles,
+						isMobileMenuOpen && !isAboveBreakpoint ? "opacity-0" : "opacity-100",
+					)}
+				/>
+				<div
+					className={twMerge(
+						itemStyles,
+						isMobileMenuOpen && !isAboveBreakpoint && "-translate-y-2 -rotate-45",
+					)}
+				/>
 			</button>
-		</>
+		</ReactFocusLock>
 	);
 };
