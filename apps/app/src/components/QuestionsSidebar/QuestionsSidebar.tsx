1235b021d94885f66b90fdb6e594cad74d035f0d
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-01-06 15:53:08 +0100

    feat(app): add focus trap in 'QuestionsSidebar'

--- apps/app/src/components/QuestionsSidebar/QuestionsSidebar.tsx
+++ apps/app/src/components/QuestionsSidebar/QuestionsSidebar.tsx
@@ -2,30 +2,35 @@
 
 import { useEffect } from "react";
 import { twMerge } from "tailwind-merge";
+import ReactFocusLock from "react-focus-lock";
 import { useUIContext } from "../../providers/UIProvider";
 import { Button } from "../Button/Button";
 import { CloseButton } from "../CloseButton/CloseButton";
 import { lockScroll, unlockScroll } from "../../utils/pageScroll";
+import { useIsAboveBreakpoint } from "../../hooks/useIsAboveBreakpoint";
 import { LevelFilter } from "./LevelFilter/LevelFilter";
 import { TechnologyFilter } from "./TechnologyFilter/TechnologyFilter";
 
 export const QuestionsSidebar = () => {
 	const { isSidebarOpen, closeSidebar } = useUIContext();
+	const isAboveBreakpoint = useIsAboveBreakpoint({ breakpoint: 640 });
 
 	useEffect(() => {
 		if (isSidebarOpen) {
-			lockScroll({ mobileOnly: true });
+			lockScroll({ mobileOnly: true, preventLayoutShift: false });
 		} else {
-			unlockScroll({ mobileOnly: true });
+			unlockScroll({ mobileOnly: true, preventLayoutShift: false });
 		}
 	}, [isSidebarOpen]);
 
 	return (
-		<aside
+		<ReactFocusLock
+			disabled={!isSidebarOpen || isAboveBreakpoint}
 			className={twMerge(
 				"border-aside fixed inset-0 z-50 flex shrink-0 -translate-x-full transform-gpu flex-col overflow-y-auto bg-gray-50 px-2.5 py-6 pb-4 transition-transform duration-200 dark:bg-neutral-800 sm:sticky sm:top-[56px] sm:z-auto sm:h-[calc(100vh-56px)] sm:w-62 sm:translate-x-0 sm:border-r-8 sm:pl-0 sm:pr-10 sm:transition-none sm:after:inline",
 				isSidebarOpen && "translate-x-0",
 			)}
+			as="aside"
 		>
 			<TechnologyFilter />
 			<LevelFilter />
@@ -33,6 +38,6 @@ export const QuestionsSidebar = () => {
 				Pokaż wyniki
 			</Button>
 			<CloseButton className="absolute top-1 right-1 sm:hidden" onClick={closeSidebar} />
-		</aside>
+		</ReactFocusLock>
 	);
 };
