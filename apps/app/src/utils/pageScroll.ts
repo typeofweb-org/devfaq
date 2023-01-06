720e88aac1872182b29e69561d8602d68c9c369f
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-01-06 15:18:39 +0100

    refactor(app): refactor 'pageScroll' utility file

--- apps/app/src/utils/pageScroll.ts
+++ apps/app/src/utils/pageScroll.ts
@@ -1,7 +1,14 @@
 import "client-only";
 
-export const lockScroll = ({ mobileOnly }: { mobileOnly: boolean }) => {
-	document.body.style.paddingRight = `${window.innerWidth - document.body.offsetWidth}px`;
+type PropsType = {
+	mobileOnly: boolean;
+	preventLayoutShift: boolean;
+};
+
+export const lockScroll = ({ mobileOnly, preventLayoutShift }: PropsType) => {
+	if (preventLayoutShift) {
+		document.body.style.paddingRight = `${window.innerWidth - document.body.offsetWidth}px`;
+	}
 
 	if (mobileOnly) {
 		document.body.classList.add("overflow-hidden", "sm:overflow-auto");
@@ -10,8 +17,10 @@ export const lockScroll = ({ mobileOnly }: { mobileOnly: boolean }) => {
 	}
 };
 
-export const unlockScroll = ({ mobileOnly }: { mobileOnly: boolean }) => {
-	document.body.style.paddingRight = "";
+export const unlockScroll = ({ mobileOnly, preventLayoutShift }: PropsType) => {
+	if (preventLayoutShift) {
+		document.body.style.paddingRight = "";
+	}
 
 	if (mobileOnly) {
 		document.body.classList.remove("overflow-hidden", "sm:overflow-auto");
