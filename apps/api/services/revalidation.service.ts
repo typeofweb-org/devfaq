f8683c6e6e1f97e848d4084a38f8f0bb4ecda219
adipol1359 <adrian.polak20@gmail.com>
2023-01-08 19:15:50 +0100

    refactor(api): remove test code

--- apps/api/services/revalidation.service.ts
+++ apps/api/services/revalidation.service.ts
@@ -1,12 +1,9 @@
 import { getConfig } from "../config/config.js";
 
-export const revalidate = (path: string) => {
-	console.log(getConfig("APP_URL"));
-
-	return fetch(
+export const revalidate = (path: string) =>
+	fetch(
 		`${getConfig("APP_URL")}/api/revalidation?token=${getConfig(
 			"REVALIDATION_TOKEN",
 		)}&path=${path}`,
 		{ method: "POST" },
 	);
-};
