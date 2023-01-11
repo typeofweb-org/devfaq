d0e9f43d23295024394814308eb7eb8e29a22d0c
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-01-11 23:21:25 +0100

    feat(app): add focus trap to main nav and QuestionsSidebar; add small fixes to lockScroll (#489)
    * feat(app): add 'useIsAboveBreakpoint' hook
    
    * refactor(app): refactor 'pageScroll' utility file
    
    * feat(app): add focus lock in HeaderNavigation
    
    * fix(app): changes in BaseModal to reflect changes in pageScroll
    
    * feat(app): add focus trap in 'QuestionsSidebar'
    
    * fix(app): fix hamburger button position
    
    * fix scrollbar gutter
    
    * Update apps/app/src/utils/pageScroll.ts
    
    Co-authored-by: Michał Miszczyszyn <michal@mmiszy.pl>
    
    * Update apps/app/src/utils/pageScroll.ts
    
    Co-authored-by: Michał Miszczyszyn <michal@mmiszy.pl>
    
    * refactor(app): change types declarations in 'pageScroll'
    
    * refactor(app): refactor styles related to scrollbar on page
    
    * Update apps/app/src/components/Header/HeaderNavigation.tsx
    
    Co-authored-by: Michał Miszczyszyn <michal@mmiszy.pl>
    
    * refactor(app): refactor 'HeaderNavigation'
    
    * fix(app): fix typo
    
    Co-authored-by: Michał Miszczyszyn <michal@mmiszy.pl>

--- apps/app/src/utils/pageScroll.ts
+++ apps/app/src/utils/pageScroll.ts
@@ -1,20 +1,26 @@
 import "client-only";
 
 export const lockScroll = ({ mobileOnly }: { mobileOnly: boolean }) => {
-	document.body.style.paddingRight = `${window.innerWidth - document.body.offsetWidth}px`;
+	// @todo remove this if statement when safari start support 'scrollbar-gutter' css property
+	if (navigator.userAgent.includes("Safari") && !navigator.userAgent.includes("Chrome")) {
+		document.body.style.paddingRight = `${window.innerWidth - document.body.offsetWidth}px`;
+	}
 
 	if (mobileOnly) {
-		document.body.classList.add("overflow-hidden", "sm:overflow-auto");
+		document.body.classList.add("overflow-hidden", "sm:overflow-y-auto");
 	} else {
 		document.body.classList.add("!overflow-hidden");
 	}
 };
 
 export const unlockScroll = ({ mobileOnly }: { mobileOnly: boolean }) => {
-	document.body.style.paddingRight = "";
+	// @todo remove this if statement when safari start support 'scrollbar-gutter' css property
+	if (navigator.userAgent.includes("Safari") && !navigator.userAgent.includes("Chrome")) {
+		document.body.style.paddingRight = "";
+	}
 
 	if (mobileOnly) {
-		document.body.classList.remove("overflow-hidden", "sm:overflow-auto");
+		document.body.classList.remove("overflow-hidden", "sm:overflow-y-auto");
 	} else {
 		document.body.classList.remove("!overflow-hidden");
 	}
