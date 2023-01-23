e87aebb6be80ae92ca07c20acddd1a224e16d14b
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-01-23 19:51:32 +0100

    implement "additionalActionOnClose" function in dashboard modals

--- apps/app/src/components/UserQuestions/UserQuestionLeftSection.tsx
+++ apps/app/src/components/UserQuestions/UserQuestionLeftSection.tsx
@@ -15,18 +15,17 @@ export const UserQuestionLeftSection = ({
 }: UserQuestionLeftSectionProps) => {
 	const { openModal, openedModal } = useUIContext();
 
-	useEffect(() => {
-		if (!openedModal) {
-			refetchQuestions();
-		}
-	}, [openedModal, refetchQuestions]);
-
 	return (
 		<div className="mr-3 flex flex-col justify-start gap-1.5">
 			<Button
 				variant="branding"
 				className="m-px flex w-24 min-w-0 items-center justify-center gap-2 p-0"
-				onClick={() => openModal("AddQuestionModal", question)}
+				onClick={() =>
+					openModal("AddQuestionModal", {
+						...question,
+						additionalActionOnClose: refetchQuestions,
+					})
+				}
 			>
 				<PencilIcon className="fill-violet-700 dark:fill-neutral-200" />
 				Edytuj
